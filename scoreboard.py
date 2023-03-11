60ffcb54291a5d380ee6613379eec09f5e108606
“Nebil <nebilsolomon@csu.fullerton.edu>
2023-03-11 12:09:18 -0800

    level is show up on board

--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,10 @@ class Scoreboard:
     def __init__(self, game): 
         self.score = 0
         self.level = 0
+        self.game = game
+       # self.level = self.game.ship.ships_left
+        #game.ship.ships_left
+
         self.high_score = 0
         
         self.settings = game.settings
@@ -60,7 +64,7 @@ class Scoreboard:
     def prep_score(self): 
 
         score_str = str(self.score)
-        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+        self.score_image = self.font.render("Score = " + str(score_str), True, self.text_color, self.settings.bg_color)
 
         # Display the score at the top right of the screen.
         self.score_rect = self.score_image.get_rect()
@@ -70,10 +74,20 @@ class Scoreboard:
 
         high_score_str  = str(self.high_score)
 
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+        self.high_score_image = self.font.render("High Score = " + str(high_score_str), True, self.text_color, self.settings.bg_color)
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.center = self.screen_rect.center 
         self.high_score_rect.top = 20
+
+         # Display level  the screen.
+
+        self.level_image = self.font.render("Level = " + str(self.level), True, self.text_color, self.settings.bg_color)
+
+        self.level_rect = self.level_image.get_rect()
+
+        self.level_rect.right = self.screen_rect.right - 20
+
+        self.level_rect.top = 50
 
 
 
@@ -88,3 +102,4 @@ class Scoreboard:
     def draw(self): 
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
