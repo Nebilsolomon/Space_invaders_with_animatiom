ce961fe7bd35065e2d51e9e6b7bca1c60566573e
“Nebil <nebilsolomon@csu.fullerton.edu>
2023-03-10 19:33:18 -0800

    menu is working now

--- game.py
+++ game.py
@@ -16,13 +16,11 @@ class Game:
         self.settings = Settings()
 
 
-        size = self.settings.screen_width, self.settings.screen_height   # tuple
-        self.myScreen = pg.display.set_mode(size=size)
-        pg.display.set_caption("gokdemor")
-
+       
 
 
         size = self.settings.screen_width, self.settings.screen_height   # tuple
+    
         self.screen = pg.display.set_mode(size=size)
         pg.display.set_caption("Alien Invasion xxxx")
 
@@ -37,6 +35,52 @@ class Game:
         self.aliens = Aliens(game=self)
         self.settings.initialize_speed_settings()
 
+
+
+
+
+#============================================================================
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.state = "start"
+
+        self.font_large = pg.font.Font(None, 60)
+        self.font_small = pg.font.Font(None, 30)
+        self.title_text = self.font_large.render("My Awesome Game", True, self.white)
+        self.start_text = self.font_small.render("Click PLAY to start", True, self.white)
+        self.button_width = 200
+        self.button_height = 50
+        self.button_x = self.settings.screen_width / 2 - self.button_width / 2
+        self.button_y = self.settings.screen_height / 2 - self.button_height / 2
+        self.button_text = self.font_small.render("PLAY", True, self.black)
+        self.button_text_x = self.button_x + self.button_width / 2 - self.button_text.get_width() / 2
+        self.button_text_y = self.button_y + self.button_height / 2 - self.button_text.get_height() / 2
+    
+    def draw(self):
+        self.screen.fill(self.settings.bg_color)
+        self.screen.blit(self.title_text, (self.settings.screen_width / 2 - self.title_text.get_width() / 2, 100))
+        pg.draw.rect(self.screen, self.black, (self.button_x, self.button_y, self.button_width, self.button_height))
+        self.screen.blit(self.button_text, (self.button_text_x, self.button_text_y))
+
+
+
+
+
+
+#============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
     def handle_events(self):
         keys_dir = {pg.K_w: Vector(0, -1), pg.K_UP: Vector(0, -1), 
                     pg.K_s: Vector(0, 1), pg.K_DOWN: Vector(0, 1),
@@ -45,6 +89,15 @@ class Game:
         
         for event in pg.event.get():
             if event.type == pg.QUIT: self.game_over()
+
+            elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                # Check if user clicked on PLAY button
+                if self.button_x < event.pos[0] < self.button_x + self.button_width and self.button_y < event.pos[1] < self.button_y + self.button_height:
+                    self.state = "play"
+                  #  self.game.sound.play_bg()
+        
+   
+           
             elif event.type == pg.KEYDOWN:
                 key = event.key
                 if key in keys_dir:
@@ -76,16 +129,43 @@ class Game:
         self.sound.play_bg()
         while True:     
             self.handle_events() 
-            self.myScreen.fill(self.settings.bg_color)
-            
-            self.screen.fill(self.settings.bg_color)
-            
-            self.ship.update()
-            self.aliens.update()
-            self.barriers.update()
-            # self.lasers.update()    # handled by ship for ship_lasers and by alien for alien_lasers
-            self.scoreboard.update()
-            pg.display.flip()
+
+            #self.screen.fill(self.black)
+            if self.state  == "start":
+           # if True:
+
+                self.screen.blit(self.title_text, (self.settings.screen_width / 2 - self.title_text.get_width() / 2, self.settings.screen_height / 4))
+                self.screen.blit(self.start_text, (self.settings.screen_width / 2 - self.start_text.get_width() / 2, self.settings.screen_height / 2))
+            # pg.draw.rect(self.settings.screen_width, self.white, (self.button_x, self.button_y, self.button_width, self.button_height))
+                
+                pg.draw.rect(self.screen, self.white, (self.button_x, self.button_y, self.button_width, self.button_height))
+                pg.display.update()
+
+                
+                self.screen.blit(self.button_text, (self.button_text_x, self.button_text_y))
+            elif self.state == "play":
+            # Add your game code here
+                
+
+                pg.display.update()
+
+
+
+
+
+
+
+
+
+                
+                self.screen.fill(self.settings.bg_color)
+                
+                self.ship.update()
+                self.aliens.update()
+                self.barriers.update()
+                # self.lasers.update()    # handled by ship for ship_lasers and by alien for alien_lasers
+                self.scoreboard.update()
+                pg.display.flip()
 
 
 def main():
